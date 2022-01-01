e46eedbd89c5ce6b888bcf57ce2fe9e3563ddb0c
Shaiful Islam Palash <kuvic16@gmail.com>
2022-01-02 00:04:08 +0600

    binary search

--- src/main/java/BinarySearch.java
+++ src/main/java/BinarySearch.java
@@ -3,4 +3,34 @@
  * @CreatedAt: 1/1/2022
  */
 public class BinarySearch {
+
+
+    public static int bSearch(int a[], int n, int x) {
+
+        int left, right, mid;
+        left = 0;
+        right = n - 1;
+
+        while(left <= right) {
+            mid = (left + right) / 2;
+
+            if(a[mid] == x) {
+                return mid;
+            }
+
+            if(a[mid] < x) {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1, 4, 5, 6, 9, 100, 200, 201, 209, 300};
+        System.out.println(bSearch(a, 10, 201));
+    }
 }
