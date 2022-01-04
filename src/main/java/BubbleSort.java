05020a02f984e327329b114d219239b3526c725b
Shaiful Islam Palash <kuvic16@gmail.com>
2022-01-04 21:48:11 +0600

    bubble sort

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -3,4 +3,27 @@
  * @CreatedAt: 1/4/2022
  */
 public class BubbleSort {
+
+    public static void bSort(int a[], int n) {
+        int i, j, temp;
+
+        for(i=0; i<n; i++) {
+            for(j=0; j<n-i-1; j++){
+                if(a[j] > a[j+1]) {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+
+        for (i = 0; i<n-1; i++) {
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int a[] = {1, 4, 5, 6, 9, 100, 200, 201, 209, 300};
+        bSort(a, 10);
+    }
 }
