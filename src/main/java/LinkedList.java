baded1a661daaa38fa68b945b09b11407a59da69
Shaiful Islam Palash <kuvic16@gmail.com>
2022-01-08 07:45:56 +0600

    linked list

---
+++ src/main/java/LinkedList.java
@@ -0,0 +1,12 @@
+/**
+ * @Author Shaiful Islam Palash | kuvic16@gmail.com
+ * @CreatedAt: 1/8/2022
+ */
+
+/**
+ * A linked list is a linear data structure, in which the elements are not stored at
+ * contiguous memory locations. The elements in a linked list are linked using
+ * pointers.
+ */
+public class LinkedList {
+}
