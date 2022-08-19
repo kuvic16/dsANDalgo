a1012d0785b58c7273f3a9239c60fcd408724274
Shaiful Islam Palash <islam.shaiful@bjitgroup.com>
2022-08-19 18:31:57 +0600

    BuiltIn circular queue

---
+++ src/main/java/leetcode/circular_queue/BuildInCircularQueue.java
@@ -0,0 +1,18 @@
+package leetcode.circular_queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BuildInCircularQueue {
+    public static void main(String[] args){
+        Queue<Integer> q = new LinkedList<Integer>();
+        System.out.println("The first element is: " + q.peek());
+        q.offer(5);
+        q.offer(13);
+        q.offer(8);
+        q.offer(6);
+        q.poll();
+        System.out.println("The first element is: " + q.peek());
+        System.out.println("The size is: " + q.size());
+    }
+}
