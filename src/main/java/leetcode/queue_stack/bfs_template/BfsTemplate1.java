c54664099dac12708a515d676102b7e579113ea8
Shaiful Islam Palash <islam.shaiful@bjitgroup.com>
2022-08-21 11:22:11 +0600

    Bfs template

--- src/main/java/leetcode/queue_stack/bfs_template/BfsTemplate1.java
+++ src/main/java/leetcode/queue_stack/bfs_template/BfsTemplate1.java
@@ -7,16 +7,12 @@ import java.util.Queue;
 
 public class BfsTemplate1 {
     static class Node {
-        int data;
+        char data;
         List<Node> neigbours;
 
-        Node(int d, int[] ngs) {
+        Node(char d, List<Node> _neigbours) {
             data = d;
-            neigbours = new ArrayList<Node>();
-            for(int n : ngs){
-                int[] nn = {};
-                neigbours.add(new Node(n, nn));
-            }
+            neigbours = _neigbours;
         }
     }
 
@@ -44,7 +40,23 @@ public class BfsTemplate1 {
 
 class BfsTemplate1Test {
     public static void main(String[] args){
-        BfsTemplate1.Node node = new BfsTemplate1.Node(1, new int[]{2, 3, 4});
+        char[][] grid = new char[4][5];
+        grid[0] = new char[]{'1','1','1','1','0'};
+        grid[1] = new char[]{'1','1','0','1','0'};
+        grid[2] = new char[]{'1','1','0','0','0'};
+        grid[3] = new char[]{'0','0','0','0','0'};
+
+        System.out.println(grid.length);
+        for (int i=grid.length - 1; i >=0; i--){
+            for(int j=grid[i].length - 1; j >= 0; j--){
+                List<BfsTemplate1.Node> neigbours = new ArrayList<BfsTemplate1.Node>();
+                if(j+1 <= grid[i].length - 1){
+                    neigbours.add(new BfsTemplate1.Node(grid[i][j+1], null));
+                }
+            }
+        }
+
+        //BfsTemplate1.Node node = new BfsTemplate1.Node('1', null);
         System.out.println("done");
     }
 }
