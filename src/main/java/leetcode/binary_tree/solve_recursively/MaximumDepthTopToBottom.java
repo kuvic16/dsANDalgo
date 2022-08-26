760befa84b93f114a16f3cd8d49f0cb4accd750a
Shaiful Islam Palash <kuvic16@gmail.com>
2022-08-26 10:12:16 +0600

    array to tree node object

--- src/main/java/leetcode/binary_tree/solve_recursively/MaximumDepthTopToBottom.java
+++ src/main/java/leetcode/binary_tree/solve_recursively/MaximumDepthTopToBottom.java
@@ -2,6 +2,9 @@ package leetcode.binary_tree.solve_recursively;
 
 import leetcode.binary_tree.TraverseTree.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MaximumDepthTopToBottom {
     private int answer;
 
@@ -18,8 +21,34 @@ public class MaximumDepthTopToBottom {
 class MaximumDepthTopToBottomTest{
     public static void main(String[] args) {
         Integer[] nodes = new Integer[]{3,9,20,null,null,15,7};
+        Queue<TreeNode> parentQueue = new LinkedList<TreeNode>();
+        TreeNode parent = null; int i=1;
+        TreeNode root = null;
         for(Integer node : nodes){
-
+            if(parent == null){
+                parent = new TreeNode(node);
+                root = parent;
+                parentQueue.offer(parent);
+                continue;
+            }
+            if(i==1){
+                if(node != null) {
+                    TreeNode curr = new TreeNode(node);
+                    parent.left = curr;
+                    parentQueue.offer(curr);
+                }
+                i += 1;
+            }else if(i == 2){
+                if(node != null) {
+                    TreeNode curr = new TreeNode(node);
+                    parent.right = curr;
+                    parentQueue.offer(curr);
+                }
+                i = 1;
+                parentQueue.poll();
+                parent = parentQueue.peek();
+            }
         }
+        System.out.println();
     }
 }
