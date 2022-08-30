a59c8aeba9b54c61f8d8e026f9199653d26e71de
Shaiful Islam Palash <kuvic16@gmail.com>
2022-08-31 03:15:19 +0600

    Construct binary tree problem accepted

--- src/main/java/leetcode/binary_tree/conclusion/ConstructBTreeFromInPost.java
+++ src/main/java/leetcode/binary_tree/conclusion/ConstructBTreeFromInPost.java
@@ -7,75 +7,87 @@ import java.util.List;
 
 public class ConstructBTreeFromInPost {
 
-    private int currIndex = 0;
+    TreeNode root = null;
 
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        TreeNode root = new TreeNode();
-        currIndex = postorder.length - 1;
-        buildTree(inorder, postorder, root);
+        buildBTree(inorder, postorder, root, true, postorder.length-1);
         return root;
     }
 
-    private void buildTree(int[] list, int[] postorder, TreeNode parent){
-        if(currIndex > 0) {
-            int curr = postorder[currIndex--];
-            List<Integer> left = new ArrayList<Integer>();
-            List<Integer> right = new ArrayList<Integer>();
-            boolean found = false;
-            for (int i : list) {
-                if (i == curr) {
-                    found = true;
-                    continue;
-                }
-                if (found == false) {
-                    left.add(i);
-                } else {
-                    right.add(i);
-                }
-            }
-            TreeNode node = new TreeNode(curr);
-            if (parent != null) {
-                parent.left = node;
-            }
+    private void buildBTree(int[] list, int[] postorder, TreeNode parent, boolean isLeft, int postindex){
+        int curr[] = getCurr(list, postorder, postindex);
+        if(curr[0] == -1) return;
 
-            int[] lefarr = new int[left.size()];
-            int i = 0;
-            for (Integer l : left) {
-                lefarr[i++] = l;
-            }
+        TreeNode node = new TreeNode(curr[1]);
+        if(parent != null){
+            if(isLeft) parent.left = node;
+            else parent.right = node;
+        }else {
+            root = node;
+        }
 
-            if (lefarr.length > 0) {
-                buildTree(lefarr, postorder, node);
+        if(list.length == 1) {
+            return;
+        }
+
+
+        int left[] = new int[curr[0]];
+        for(int i=0; i<curr[0]; i++){
+            left[i] = list[i];
+            //System.out.print(list[i] + " ");
+        }
+        //System.out.println();
+        buildBTree(left, postorder, node,true, postindex-1);
+
+        if(list.length -1 - curr[0] > 0) {
+            int right[] = new int[list.length - 1 - curr[0]];
+            int j=0;
+            for(int i=curr[0]+1; i<list.length; i++){
+                right[j++] = list[i];
+                //System.out.print(list[i] + " ");
             }
+            //System.out.println();
+            buildBTree(right, postorder, node,false, postindex-1);
         }
     }
 
-    public void traverse(TreeNode root){
-        if(root != null){
-            traverse(root.left);
-            System.out.print(root.val + " ");
-            traverse(root.right);
+    private int[] getCurr(int[] list, int[] postorder, int postindex){
+        int[] curr = new int[2];
+        if(list.length == 1) {
+            curr[0] = 0;
+            curr[1] = list[0];
+            return curr;
         }
+
+        for(int i=postindex; i>=0; i--){
+            for(int j=0; j<list.length; j++){
+                if(postorder[i] == list[j]) {
+                    curr[0] = j;
+                    curr[1] = list[j];
+                    return curr;
+                }
+            }
+        }
+        curr[0] = -1;
+        curr[1] = -1;
+        return curr;
     }
 
 }
 
 class ConstructBTreeFromInPostTest{
     public static void main(String[] args) {
-//        Integer[] nodes = new Integer[]{1,2,2,3,4,4,3};
-//        Integer[] nodes = new Integer[]{1,2,3};
-//        Integer[] nodes = new Integer[]{1,2,2,null,3,null,3};
-//        Integer[] nodes = new Integer[]{2,3,3,4,5,5,4,null,null,8,9,null,null,9,8};
-//        Integer[] nodes = new Integer[]{1,2,2,2,null,2};
-//        Integer[] nodes = new Integer[]{3,9,20,null,null,15,7};
-//        TreeNode root = new TreeNode().prepareTree(nodes);
-
         ConstructBTreeFromInPost c = new ConstructBTreeFromInPost();
 
-        int[] inorder = new int[]{9,3,15,20,7};
-        int[] postorder = new int[]{9,15,7,20,3};
-        // 3,9,20,null,null,15,7
+        //int[] inorder = new int[]{9,3,15,20,7};
+        //int[] postorder = new int[]{9,15,7,20,3};
+
+        //int[] inorder = new int[]{4, 8, 2, 5, 1, 6, 3, 7};
+        //int[] postorder = new int[]{8, 4, 5, 2, 6, 7, 3, 1};
+
+        int[] inorder = new int[]{-1};
+        int[] postorder = new int[]{-1};
+
         c.buildTree(inorder, postorder);
-        //c.traverse(root);
     }
 }
