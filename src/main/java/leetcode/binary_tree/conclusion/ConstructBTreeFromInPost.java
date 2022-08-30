05b83702c5783c6fc0892bf5f3726b92b915a27c
Shaiful Islam Palash <kuvic16@gmail.com>
2022-08-31 01:04:46 +0600

    Construct binary tree is in progress

--- src/main/java/leetcode/binary_tree/conclusion/ConstructBTreeFromInPost.java
+++ src/main/java/leetcode/binary_tree/conclusion/ConstructBTreeFromInPost.java
@@ -7,10 +7,47 @@ import java.util.List;
 
 public class ConstructBTreeFromInPost {
 
+    private int currIndex = 0;
+
     public TreeNode buildTree(int[] inorder, int[] postorder) {
         TreeNode root = new TreeNode();
-
+        currIndex = postorder.length - 1;
+        buildTree(inorder, postorder, root);
         return root;
+    }
+
+    private void buildTree(int[] list, int[] postorder, TreeNode parent){
+        if(currIndex > 0) {
+            int curr = postorder[currIndex--];
+            List<Integer> left = new ArrayList<Integer>();
+            List<Integer> right = new ArrayList<Integer>();
+            boolean found = false;
+            for (int i : list) {
+                if (i == curr) {
+                    found = true;
+                    continue;
+                }
+                if (found == false) {
+                    left.add(i);
+                } else {
+                    right.add(i);
+                }
+            }
+            TreeNode node = new TreeNode(curr);
+            if (parent != null) {
+                parent.left = node;
+            }
+
+            int[] lefarr = new int[left.size()];
+            int i = 0;
+            for (Integer l : left) {
+                lefarr[i++] = l;
+            }
+
+            if (lefarr.length > 0) {
+                buildTree(lefarr, postorder, node);
+            }
+        }
     }
 
     public void traverse(TreeNode root){
@@ -37,6 +74,7 @@ class ConstructBTreeFromInPostTest{
 
         int[] inorder = new int[]{9,3,15,20,7};
         int[] postorder = new int[]{9,15,7,20,3};
+        // 3,9,20,null,null,15,7
         c.buildTree(inorder, postorder);
         //c.traverse(root);
     }
