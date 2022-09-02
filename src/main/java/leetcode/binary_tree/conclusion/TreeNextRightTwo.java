adf1d9741352f66748751a93064d61e17713f161
Shaiful Islam Palash <kuvic16@gmail.com>
2022-09-02 23:45:14 +0600

    populating next right pointer II solution accepted

--- src/main/java/leetcode/binary_tree/conclusion/TreeNextRightTwo.java
+++ src/main/java/leetcode/binary_tree/conclusion/TreeNextRightTwo.java
@@ -14,35 +14,38 @@ public class TreeNextRightTwo {
         if(root == null) return null;
 
         queue.offer(root);
-        Node curr = root;
+
+        List<List<Node>> levelList = new ArrayList<List<Node>>();
         List<Node> neighbours = new ArrayList<Node>();
         neighbours.add(root);
+        levelList.add(neighbours);
 
+        Node curr = null;
+        neighbours = new ArrayList<Node>();
         while(!queue.isEmpty()){
             curr = queue.peek();
             if(curr.left != null) {
                 neighbours.add(curr.left);
-                queue.offer(curr.left);
             }
             if(curr.right != null) {
                 neighbours.add(curr.right);
-                queue.offer(curr.right);
             }
             queue.poll();
+
+            if(queue.isEmpty() && neighbours.size() > 0){
+                for(Node node : neighbours){
+                    queue.offer(node);
+                }
+                levelList.add(neighbours);
+                neighbours = new ArrayList<Node>();
+            }
         }
 
-        int counter = 0; double nextLevel = 1;
-        for(int i=0; i<neighbours.size(); i++){
-            Node node = neighbours.get(i);
-            counter += 1;
-            if(nextLevel > 1 && counter < nextLevel){
-                if(i < neighbours.size() - 1){
-                    node.next = neighbours.get(i + 1);
+        for(List<Node> level : levelList){
+            for(int i=0; i<level.size(); i++){
+                if(i+1 < level.size()){
+                    level.get(i).next = level.get(i+1);
                 }
-            }
-            if(counter == nextLevel){
-                counter = 0;
-                nextLevel = nextLevel * 2;
             }
         }
         return root;
@@ -54,11 +57,11 @@ public class TreeNextRightTwo {
 
 class TreeNextRightTreeNextRightTwoTest {
     public static void main(String[] args){
-        Integer[] nodes = new Integer[]{1,2,3,4,5,6,7};
+        Integer[] nodes = new Integer[]{1,2,3,4,5,null,7};
         Node root = new Node ();
         root = root.prepareTree(nodes);
 
-        TreeNextRight btpt = new TreeNextRight();
+        TreeNextRightTwo btpt = new TreeNextRightTwo();
         btpt.connect(root);
         //btpt.levelOrder(node5);
 
