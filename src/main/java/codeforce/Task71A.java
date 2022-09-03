42d54c10aab058674cc8fa371457f39cf42cc20a
Shaiful Islam Palash <kuvic16@gmail.com>
2022-09-03 22:00:25 +0600

    cf-71A accepted

---
+++ src/main/java/codeforce/Task71A.java
@@ -0,0 +1,20 @@
+package codeforce;
+
+import java.util.Scanner;
+
+public class Task71A {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=0; i<n; i++){
+            String word = in.next();
+            int length = word.length();
+
+            if(length <= 10){
+                System.out.println(word);
+            }else{
+                System.out.println(word.substring(0, 1) + (length - 2) + word.substring(length-1, length));
+            }
+        }
+    }
+}
