f4dde6931360a703eba259cc011d26781bbf5baa
Shaiful Islam Palash <kuvic16@gmail.com>
2022-09-03 14:31:11 +0600

    lowest common ancestor solution accepted

--- src/main/java/leetcode/binary_tree/conclusion/LowestCommonAncestor.java
+++ src/main/java/leetcode/binary_tree/conclusion/LowestCommonAncestor.java
@@ -4,37 +4,38 @@ import leetcode.binary_tree.TraverseTree.TreeNode;
 
 public class LowestCommonAncestor {
 
+    private TreeNode ancestor = null;
     private boolean pPassed = false;
     private boolean qPassed = false;
-    private TreeNode currentP = null;
-    private TreeNode currentQ = null;
-    private TreeNode ancestor = null;
-
 
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        traverse(root, p.val, q.val, 1, 0, 0);
+        traverse(root, p.val, q.val);
         return ancestor;
     }
 
-    private void traverse(TreeNode root, int p, int q, int level, int pStep, int qStep){
-        if(root != null && ancestor == null){
+    private void traverse(TreeNode root, int p, int q){
+        if(root != null && (root.left != null || root.right != null) && ancestor == null) {
+            traverse(root.left, p, q);
+            traverse(root.right, p, q);
+            //System.out.print(root.val + " ");
+            if(ancestor == null) {
+                pPassed = false;
+                qPassed = false;
+                check(root, p, q);
+                if (pPassed && qPassed) {
+                    ancestor = root;
+                }
+            }
+        }
+    }
 
-            traverse(root.left, p, q, level + 1, pStep, qStep);
-            traverse(root.right, p, q, level + 1, pStep, qStep);
-            //System.out.print(root.val + "(" + level + "-" + left + "," + right + ")" + " ");
-            System.out.print(root.val + "(" + pStep + "-" + qStep + ")" + " ");
-            if(pPassed && qPassed && ancestor == null){
-                ancestor = root;
-            }
-            if(root.val == p) {
-                pPassed = true;
-                currentP = root;
-            }
-            if(root.val == q) {
-                qPassed = true;
-                currentQ = root;
-            }
+    private void check(TreeNode root, int p, int q){
+        if(root != null && (!pPassed || !qPassed)){
+            if(root.val == p) pPassed = true;
+            if(root.val == q) qPassed = true;
+            check(root.left, p, q);
+            check(root.right, p, q);
         }
     }
 }
@@ -64,17 +65,17 @@ public class LowestCommonAncestor {
 
 class LowestCommonAncestorTest {
     public static void main(String[] args){
-//        Integer[] nodes = new Integer[]{3,5,1,6,2,0,8,null,null,7,4};
-//        TreeNode root = new TreeNode();
-//        root = root.prepareTree(nodes);
-//        TreeNode p = new TreeNode(5);
-//        TreeNode q = new TreeNode(1);
-
         Integer[] nodes = new Integer[]{3,5,1,6,2,0,8,null,null,7,4};
         TreeNode root = new TreeNode();
         root = root.prepareTree(nodes);
         TreeNode p = new TreeNode(5);
-        TreeNode q = new TreeNode(4);
+        TreeNode q = new TreeNode(1);
+
+//        Integer[] nodes = new Integer[]{3,5,1,6,2,0,8,null,null,7,4};
+//        TreeNode root = new TreeNode();
+//        root = root.prepareTree(nodes);
+//        TreeNode p = new TreeNode(5);
+//        TreeNode q = new TreeNode(4);
 
         LowestCommonAncestor btpt = new LowestCommonAncestor();
         TreeNode result = btpt.lowestCommonAncestor(root, p, q);
