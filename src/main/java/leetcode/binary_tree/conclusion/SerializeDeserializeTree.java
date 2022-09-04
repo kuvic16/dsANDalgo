aa512984473ba01ce5f881fae671b4cf7ef1fdd2
Shaiful Islam Palash <kuvic16@gmail.com>
2022-09-04 14:09:23 +0600

    LC: serialize and deserialize the tree in progress

---
+++ src/main/java/leetcode/binary_tree/conclusion/SerializeDeserializeTree.java
@@ -0,0 +1,29 @@
+package leetcode.binary_tree.conclusion;
+
+import leetcode.binary_tree.Node;
+import leetcode.binary_tree.TraverseTree.TreeNode;
+
+
+public class SerializeDeserializeTree {
+
+    public String serialize(TreeNode root) {
+        return null;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return null;
+    }
+
+}
+
+
+class SerializeDeserializeTreeTest {
+    public static void main(String[] args){
+        Integer[] nodes = new Integer[]{1,2,3,4,5,null,7};
+        Node root = new Node ();
+        root = root.prepareTree(nodes);
+
+        SerializeDeserializeTree btpt = new SerializeDeserializeTree();
+    }
+}
