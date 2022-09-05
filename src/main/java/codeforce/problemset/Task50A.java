8b2efa5515501ebdd62cc92bf1f9452646c601c8
Shaiful Islam Palash <kuvic16@gmail.com>
2022-09-05 13:19:06 +0600

    CF: 50A accepted

---
+++ src/main/java/codeforce/problemset/Task50A.java
@@ -0,0 +1,17 @@
+package codeforce.problemset;
+
+import java.util.Scanner;
+
+public class Task50A {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+//        int m = 3, n = 3;
+        int x = 2;
+        int y = 1;
+
+        int result = ((m/x) * (n * y)) + (((m%x) * (n*y))/x);
+        System.out.println(result);
+    }
+}
