cb26a8b0e0a924cf4d012937f79b9c9b5fa18499
Shaiful Islam Palash <kuvic16@gmail.com>
2022-09-11 20:41:02 +0600

    minro changes made

--- src/main/java/codeforce/problemset/a/Task112A.java
+++ src/main/java/codeforce/problemset/a/Task112A.java
@@ -5,24 +5,47 @@ import java.util.Scanner;
 public class Task112A {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		String s1 = in.next();
-		String s2 = in.next();
-		
-		int s1sum = 0, s2sum = 0;
-		for(int c : s1.toLowerCase().toCharArray()) {
-			s1sum += c;
+//		String s1 = in.next();
+//		String s2 = in.next();
+//		String s1 = "ttXjenUAlfixytHEOrPkgXmkKTSGYuyVXGIHYmWWYGlBYpHkujueqBSgjLguSgiMGJWATIGEUjjAjKXdMiVbHozZUmqQtFrT";
+//		String s2 = "JziDBFBDmDJCcGqFsQwDFBYdOidLxxhBCtScznnDgnsiStlWFnEXQrJxqTXKPxZyIGfLIToETKWZBPUIBmLeImrlSBWCkTNo";
+		String s1 = "aslkjlkasdd";
+		String s2 = "asdlkjdajwi";
+
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+
+		char[] s1s = s1.toCharArray();
+		char[] s2s = s2.toCharArray();
+
+		for(int i=0; i<s1s.length; i++){
+			for(int j=i+1; j<s1s.length; j++){
+				if(s1s[i] > s1s[j]){
+					char tmp = s1s[i];
+					s1s[i] = s1s[j];
+					s1s[j] = tmp;
+				}
+				if(s2s[i] > s2s[j]){
+					char tmp = s2s[i];
+					s2s[i] = s2s[j];
+					s2s[j] = tmp;
+				}
+			}
 		}
-		//System.out.println(s1sum);
-		
-		for(int c : s2.toLowerCase().toCharArray()) {
-			s2sum += c;
+		System.out.println(s1s);
+		System.out.println(s2s);
+
+		String result = "0";
+		for(int i=0; i<s1s.length; i++){
+			if((int)s1s[i] > (int)s2s[i]){
+				result = "1";
+				break;
+			}
+			else if((int)s1s[i] < (int)s2s[i]){
+				result = "-1";
+				break;
+			}
 		}
-		//System.out.println(s2sum);
-		
-		String result = "";
-		if(s1sum < s2sum) result = "-1";
-		else if(s1sum == s2sum) result = "0";
-		else result = "1";
 		
 		System.out.println(result);
 	}
