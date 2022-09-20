c33520f3f2bc44fc0b4069ec5d13badb0da9dc6f
Shaiful Islam Palash <kuvic16@gmail.com>
2022-09-21 00:52:07 +0600

    LC: 3228 accepted

--- src/main/java/leetcode/array101/LC3228.java
+++ src/main/java/leetcode/array101/LC3228.java
@@ -2,25 +2,26 @@ package leetcode.array101;
 
 public class LC3228 {
     public int heightChecker(int[] heights) {
-        int count = 0, ln = 0;
+        int count = 0;
         int n = heights.length;
-        int[] tmpHeights = heights;
+        int[] tmpHeights = new int[n];
+        for(int i=0; i<n; i++){
+            tmpHeights[i] = heights[i];
+        }
+
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 if(tmpHeights[i] > tmpHeights[j]){
-                    int tmp = heights[i];
+                    int tmp = tmpHeights[i];
                     tmpHeights[i] = tmpHeights[j];
                     tmpHeights[j] = tmp;
                 }
             }
         }
-//        for(int i=0; i<heights.length; i++){
-//            if(i == 0 || heights[i] == ln || heights[i] == ln + 1){
-//                ln = heights[i];
-//            }else{
-//                count += 1;
-//            }
-//        }
+
+        for(int i=0; i<heights.length; i++){
+            if(heights[i] != tmpHeights[i]) count += 1;
+        }
         return count;
     }
 }
@@ -28,7 +29,9 @@ public class LC3228 {
 class LC3228Test{
     public static void main(String[] args){
         LC3228 lc3228 = new LC3228();
-        int[] h = new int[]{1,1,4,2,1,3};
+//        int[] h = new int[]{1,1,4,2,1,3};
+//        int[] h = new int[]{5,1,2,3,4};
+        int[] h = new int[]{1,2,3,4,5};
         int result = lc3228.heightChecker(h);
         System.out.println(result);
     }
