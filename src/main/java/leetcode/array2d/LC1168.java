16544a0d32d8b076b52ff03b87aaad564f0b1669
Shaiful Islam Palash <kuvic16@gmail.com>
2022-10-04 00:42:27 +0600

    LC: 1168 accepted

--- src/main/java/leetcode/array2d/LC1168.java
+++ src/main/java/leetcode/array2d/LC1168.java
@@ -8,32 +8,43 @@ public class LC1168 {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
         String cd = direction.TOP.toString();
-        int x1=0, x2= matrix.length-1, y1=0, y2 = matrix[0].length;
+        int t=0, b= matrix.length-1, l=0, r=matrix[0].length-1;
         int i=0, j=0;
-        int m=matrix.length-1, n=matrix[0].length;
-        while(x1 != x2 && y1 != y2){
+        int m=matrix.length, n=matrix[0].length;
+        while(result.size() < m*n && (t != b || l != r)){
             result.add(matrix[i][j]);
             if(cd.equalsIgnoreCase(direction.TOP.toString())){
-                if(j < n) j++;
-                if(j == n-1){
-                    cd = direction.LEFT.toString();
-                }
-            }else if(cd.equalsIgnoreCase(direction.LEFT.toString())){
-                if(i < m) i++;
-                if(i == m-1){
-                    cd = direction.BOTTOM.toString();
-                }
-            }else if(cd.equalsIgnoreCase(direction.BOTTOM.toString())){
-                if(j >= 0) j--;
-                if(j == 0){
+                if(j < r) j++;
+                else if(j == r){
                     cd = direction.RIGHT.toString();
-                }
+                    t++;
+                    i++;
+                }else break;
             }else if(cd.equalsIgnoreCase(direction.RIGHT.toString())){
-                if(j >= 0) j--;
-                if(j == 0){
-                    cd = direction.RIGHT.toString();
-                }
+                if(i < b) i++;
+                else if(i == b){
+                    cd = direction.BOTTOM.toString();
+                    r--;
+                    j--;
+                }else break;
+            }else if(cd.equalsIgnoreCase(direction.BOTTOM.toString())){
+                if(j > l) j--;
+                else if(j == l){
+                    cd = direction.LEFT.toString();
+                    b--;
+                    i--;
+                }else break;
+            }else if(cd.equalsIgnoreCase(direction.LEFT.toString())){
+                if(i > t) i--;
+                else if(i == t){
+                    cd = direction.TOP.toString();
+                    l++;
+                    j++;
+                }else break;
             }
+        }
+        if(result.size() < m*n && i<=m && j<=n){
+            result.add(matrix[i][j]);
         }
 
         return result;
@@ -43,7 +54,8 @@ public class LC1168 {
 class LC1168Test {
     public static void main(String[] args){
         LC1168 lc1168 = new LC1168();
-        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+//        int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        int[][] matrix = new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         List<Integer> result = lc1168.spiralOrder(matrix);
         System.out.println();
         for(Integer r : result) System.out.print(r + " ");
