bcdecbba1dcfaeb79243dae4cec8c0396558116e
Shaiful Islam Palash <kuvic16@gmail.com>
2023-08-15 10:10:25 +0600

    LC1431 accepted

---
+++ src/main/java/leetcode/array_and_string/LC1431.java
@@ -0,0 +1,34 @@
+package leetcode.array_and_string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC1431 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> result = new ArrayList<>();
+        int i=0, j = candies.length-1, max = 0;
+        while(i<=j){
+            if(candies[i] > max) max = candies[i];
+            if(candies[j] > max) max = candies[j];
+            i++; j--;
+        }
+
+        for(i = 0; i<candies.length; i++){
+            result.add((candies[i] + extraCandies) >= max);
+        }
+
+        // for(i=0; i<candies.length-1; i++){
+            
+        // }
+        // System.out.println(max);
+        return result;
+    }
+}
+
+class LC1431Test {
+    public static void main(String[] args){
+        LC1431 lc1431 = new LC1431();
+        List<Boolean> result = lc1431.kidsWithCandies(new int[]{2,3,5,1,3}, 3);
+        for(Boolean b : result) System.out.print(b + " ");
+    }
+}
