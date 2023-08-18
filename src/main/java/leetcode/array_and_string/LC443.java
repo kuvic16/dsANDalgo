704f4b51de16baa314b237ac395b0f49372da9da
Shaiful Islam Palash <kuvic16@gmail.com>
2023-08-18 09:57:31 +0600

    LC443 accepted

--- src/main/java/leetcode/array_and_string/LC443.java
+++ src/main/java/leetcode/array_and_string/LC443.java
@@ -1,43 +1,47 @@
 package leetcode.array_and_string;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
 public class LC443 {
     public int compress(char[] chars) {
-        HashMap<String, Integer> m = new HashMap<>();
+        char cc = Character.MIN_VALUE; int count = 0, i = 0;
         for(char c : chars){
-            Integer cnt = m.get(String.valueOf(c));
-            m.put(String.valueOf(c), cnt != null ? cnt + 1 : Integer.valueOf(1));
+            if(cc == 0){
+                count = 1;
+                chars[i++] = c;
+            }else if(cc != c){
+                if(count > 1){
+                    String s = String.valueOf(count);
+                    for(char lc : s.toCharArray()){
+                        chars[i++] = lc;
+                    }
+                }
+                chars[i++] = c;
+                count = 1;                
+            }else{
+                count += 1;
+            }
+            cc = c;
         }
 
-        int t = 0, i = 0;
-        Iterator it = m.entrySet().iterator();
-        while (it.hasNext()) {
-            Map.Entry<String, Integer> pair = (Map.Entry<String, Integer>)it.next();
-            chars[i++] = pair.getKey().charAt(0);
-            t += 1;
-            if(pair.getValue() != 1) {
-                String number = String.valueOf(pair.getValue());
-                t += number.length();
-                for(char cc : number.toCharArray()){
-                    chars[i++] = cc;
-                }                
-            }            
-            it.remove(); 
-        }        
-        return t;
+        if(count > 1){
+            String s = String.valueOf(count);
+            for(char lc : s.toCharArray()){
+                chars[i++] = lc;
+            }
+        }
+
+        for(char x : chars) System.out.print(x + " ");
+        
+        
+        return i;
     }
 }
 
 class LC443Test {
     public static void main(String[] args){
         LC443 lc443 = new LC443();
-        //System.out.println(lc443.compress(new char[]{'a','a','b','b','c','c','c'}));
+        System.out.println(lc443.compress(new char[]{'a','a','b','b','c','c','c'}));
         //System.out.println(lc443.compress(new char[]{'a'}));
         //System.out.println(lc443.compress(new char[]{'a','b','b','b','b','b','b','b','b','b','b','b','b'}));
-        System.out.println(lc443.compress(new char[]{'a','a','a','b','b','a','a'}));
+        //System.out.println(lc443.compress(new char[]{'a','a','a','b','b','a','a'}));
     }
 }
