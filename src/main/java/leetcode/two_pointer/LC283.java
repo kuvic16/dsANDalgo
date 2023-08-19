ad10aab755601301233086c7828043c020a6e3c2
Shaiful Islam Palash <kuvic16@gmail.com>
2023-08-19 18:31:51 +0600

    LC283: optimal solution included by help of editorial

--- src/main/java/leetcode/two_pointer/LC283.java
+++ src/main/java/leetcode/two_pointer/LC283.java
@@ -12,7 +12,7 @@ public class LC283 {
         }
     }
 
-    public void moveZeroes(int[] nums) {
+    public void moveZeroesx(int[] nums) {
         int i = 0, j = nums.length-1; boolean moving = false;
         while(j >= 0){
             if(!moving){
@@ -39,11 +39,25 @@ public class LC283 {
 
         for(int n : nums) System.out.print(n + " ");
     }
+
+    public void moveZeroes(int[] nums) {
+        int right = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[right++];
+                nums[right-1] = temp;
+            }
+        }
+        //for(int i=right; i<nums.length; i++) nums[i] = 0;
+
+        for(int n : nums) System.out.print(n + " ");
+    }
 }
 
 class LC283Test {
     public static void main(String[] args){
         LC283 lc283 = new LC283();
-        lc283.moveZeroes(new int[]{0});
+        lc283.moveZeroes(new int[]{0,1,0,3,12});
     }
 }
