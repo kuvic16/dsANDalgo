8af13a59598a852a89f8bba6ba76a7955b724f9b
Shaiful Islam Palash <kuvic16@gmail.com>
2023-08-22 08:09:16 +0600

    LC1679 - In progress

--- src/main/java/leetcode/two_pointer/LC1679.java
+++ src/main/java/leetcode/two_pointer/LC1679.java
@@ -2,20 +2,16 @@ package leetcode.two_pointer;
 
 public class LC1679 {
     public int maxOperations(int[] nums, int k) {
-        int n = nums.length, op = 0, ln = 0;
+        int n = nums.length, op = 0;
         for(int i=0; i<n; i++){
-            if(nums[i] > k) continue;
+            if(nums[i] > k || nums[i] == -1) continue;
             for(int j=i+1; j<n; j++){
-                if(nums[j] > k) continue;
-                else if(nums[i] + nums[j] == k) {
+                if(nums[j] > k || nums[i] == -1) continue;
+                else if(nums[i] != -1 && nums[i] + nums[j] == k) {
                     op += 1;
-                    int tmp = nums[ln];
-                    nums[ln] = nums[j];
-                    nums[j] = tmp;
-                    i = ln;
+                    nums[i] = nums[j] = -1;   
                     break;
-                }
-                ln = j;
+                }                
             }
         }
         return op;
@@ -26,7 +22,7 @@ class LC1679Test {
     public static void main(String[] args){
         LC1679 lc1679 = new LC1679();
         System.out.println(lc1679.maxOperations(new int[]{1,2,3,4}, 5));
-        //System.out.println(lc1679.maxOperations(new int[]{3,1,3,4,3}, 6));
-        //System.out.println(lc1679.maxOperations(new int[]{4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4}, 2));
+        System.out.println(lc1679.maxOperations(new int[]{3,1,3,4,3}, 6));
+        System.out.println(lc1679.maxOperations(new int[]{4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4}, 2));
     }
 }
