ca1ba8783900e44d167885f2764300342fd37d31
Shaiful Islam Palash <kuvic16@gmail.com>
2023-09-08 15:07:46 +0600

    LC2095 - accepted

--- src/main/java/leetcode/linked_list/LC2095.java
+++ src/main/java/leetcode/linked_list/LC2095.java
@@ -7,36 +7,51 @@ import leetcode.common.ListNode;
 
 public class LC2095 {
     public ListNode deleteMiddle(ListNode head) {
-        int n = 0;
+        List<ListNode> list = new ArrayList<>();
         ListNode tmp = head;
         while(tmp != null){
+            list.add(new ListNode(tmp.val));
             tmp = tmp.next;
-            n += 1;
         }
 
-        int m = n/2;
-        int i = 0;
-        while(head != null){
-            head = head.next;
-            i += 1;
-            if(i == m-1)
+        if(list.size() == 1) return null;
+        int m = list.size()/2;
+
+        ListNode prev = null;
+        for(int i=list.size()-1; i>=0; i--){
+            if(m != i) {
+                head = new ListNode(list.get(i).val);
+                if (prev != null) head.next = prev;
+                prev = head;
+            }
         }
         
-        return null;
+        return head;
     }
 }
 
 class LC2095Test{
     public static void main(String[] args){
         LC2095 lc2095 = new LC2095();
-        ListNode a = new ListNode(6);
-        ListNode b = new ListNode(2, a);
-        ListNode c = new ListNode(1, b);
-        ListNode d = new ListNode(7, c);
-        ListNode e = new ListNode(4, d);
-        ListNode f = new ListNode(3, e);
-        ListNode g = new ListNode(1, f);
-        ListNode result = lc2095.deleteMiddle(g);
-        System.out.println(result.val);
+//        ListNode a = new ListNode(6);
+//        ListNode b = new ListNode(2, a);
+//        ListNode c = new ListNode(1, b);
+//        ListNode d = new ListNode(7, c);
+//        ListNode e = new ListNode(4, d);
+//        ListNode f = new ListNode(3, e);
+//        ListNode g = new ListNode(1, f);
+//        ListNode result = lc2095.deleteMiddle(g);
+
+        ListNode a = new ListNode(4);
+        ListNode b = new ListNode(3, a);
+        ListNode c = new ListNode(2, b);
+        ListNode d = new ListNode(1, c);
+        ListNode result = lc2095.deleteMiddle(d);
+
+        while(result != null){
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+
     }
 }
