873555623153712d04275b6430f5e42b16d5fc94
Shaiful Islam Palash <kuvic16@gmail.com>
2023-09-11 23:17:40 +0600

    LC328 - accepted

--- src/main/java/leetcode/linked_list/LC328.java
+++ src/main/java/leetcode/linked_list/LC328.java
@@ -1,22 +1,39 @@
 package leetcode.linked_list;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 import leetcode.common.ListNode;
 
 public class LC328 {
     public ListNode oddEvenList(ListNode head) {
-        if(head.next == null) return head;
+        if(head == null || head.next == null) return head;
 
-        ListNode tmp = head;
-        //tmp = tmp.next;
-        while(tmp != null){
-            ListNode tmp1 = tmp.next;
-            if(tmp1 != null && tmp1.next != null){
-                tmp.next = tmp1.next;
-                tmp1.next = tmp.next.next;
-                tmp.next.next = tmp1;
-            }
-            tmp = tmp.next;
+        Stack<Integer> odd = new Stack<>();
+        Stack<Integer> even = new Stack<>();
+
+        int i =1;
+        while(head != null){
+            if(i++%2==0) even.add(head.val);
+            else odd.add(head.val);
+            head = head.next;
+        } 
+
+        
+        head = null;
+        while(!even.isEmpty()){        
+            ListNode listNode = new ListNode(even.pop());
+            if(head != null) listNode.next = head;
+            head = listNode;
         }
+
+        while(!odd.isEmpty()){        
+            ListNode listNode = new ListNode(odd.pop());
+            listNode.next = head;
+            head = listNode;
+        }
+
         return head;
     }
 }
@@ -24,7 +41,10 @@ public class LC328 {
 class LC328Test {
     public static void main(String[] args){
         LC328 lc328 = new LC328();
-        ListNode sample = new ListNode(new int[]{1,2,3,4,5});
+        //  ListNode sample = new ListNode(new int[]{1,2,3,4,5});
+        //ListNode sample = new ListNode(new int[]{2,1,3,5,6,4,7});
+        //  ListNode sample = new ListNode(new int[]{1,2});
+         ListNode sample = new ListNode(new int[]{1});
         ListNode result = lc328.oddEvenList(sample);
         result.print();
     }
