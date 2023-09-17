ff790e96cdc4cfa551af22023c983fa9a6798b54
Shaiful Islam Palash <kuvic16@gmail.com>
2023-09-17 18:27:25 +0600

    LC1372 - accepted

--- src/main/java/leetcode/binary_tree/dfs/LC1372.java
+++ src/main/java/leetcode/binary_tree/dfs/LC1372.java
@@ -8,30 +8,25 @@ public class LC1372 {
         if(root == null) return 0;
         if(root.left == null && root.right == null) return 0;
 
-        traverse(root, "", "");
+        traverse(root, 0, null, null);
         return max;
     }
 
-    public void traverse(TreeNode node, String rail, String dir){
+    public void traverse(TreeNode node, int sum, String dir, String pdir){
         if(node == null) return;
 
-        if(!rail.isEmpty()) rail += ",";
-        rail += dir;
-
-        String[] dirs = rail.split(",");
-        int sum = 0; String prev = "";
-        for(int i=0; i<dirs.length; i++){
-            if(!prev.isEmpty() && !dirs[i].equalsIgnoreCase(prev)) sum += 1;
-            else sum = 1;
-            prev = dirs[i];
+        if(!(dir == null && pdir == null)){
+             if(dir.equalsIgnoreCase(pdir)) {
+                sum = 1;
+            }else sum += 1;
+            if(sum > max) max = sum;
         }
-        if(sum > max) max = sum;
+       
         
-        
-        System.out.println(node.val + "(" + rail + "): , Sum: " + sum);
+        System.out.println(node.val + ": " + dir + ", " + pdir);
 
-        traverse(node.left, rail, "left");
-        traverse(node.right, rail, "right");
+        traverse(node.left, sum, "left", dir);
+        traverse(node.right, sum, "right", dir);
     }
 }
 
@@ -40,9 +35,9 @@ class LC1372Test{
         LC1372 lc1372 = new LC1372();
         TreeNode root = new TreeNode();
         root = new TreeNode().prepareTree(new Integer[]{1,null,1,1,1,null,null,1,1,null,1,null,null,null,1}); //3
-        //root = new TreeNode().prepareTree(new Integer[]{1,1,1,null,1,null,null,1,1,null,1}); //4
-        //root = new TreeNode().prepareTree(new Integer[]{1}); //0
-        //root = new TreeNode().prepareTree(new Integer[]{1,null,2,3,4,null,null,null,5});
+        root = new TreeNode().prepareTree(new Integer[]{1,1,1,null,1,null,null,1,1,null,1}); //4
+        root = new TreeNode().prepareTree(new Integer[]{1}); //0
+        root = new TreeNode().prepareTree(new Integer[]{1,null,2,3,4,null,null,null,5});
 
         System.out.println(lc1372.longestZigZag(root));
     }
