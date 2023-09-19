e343aa030260673dcddadd7d207d71a3cc97e205
Shaiful Islam Palash <kuvic16@gmail.com>
2023-09-19 15:18:21 +0600

    LC199 - accepted

--- src/main/java/leetcode/binary_tree/bfs/LC199.java
+++ src/main/java/leetcode/binary_tree/bfs/LC199.java
@@ -4,47 +4,36 @@ import leetcode.binary_tree.TraverseTree.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class LC199 {
     List<Integer> list = new ArrayList<>();
-    int depth = 0;
+    Stack<Integer> stack = new Stack<>();
     public List<Integer> rightSideView(TreeNode root) {
-        traverse(root, 0, "right");
+        traverse(root, 0);
         return list;
     }
 
-    public void traverse(TreeNode node, int c, String dir){
+    public void traverse(TreeNode node, int c){
         if(node == null) return;
-        if(dir.equalsIgnoreCase("right") && c > depth) depth = c;
-
-        System.out.println(node.val + ":::: " + c + " >>" + depth);
-
-        list.add(node.val);
-
-        traverse(node.right, c+1, "right");
-        traverse(node.left, c+1, "left");
+        if(!stack.contains(c)){
+            list.add(node.val);
+            stack.push(c);
+        }
+        traverse(node.right, c+1);
+        traverse(node.left, c+1);
     }
 
-//    public void traverse(TreeNode node, int l, int r){
-//        if(node == null) return;
-//        if(r > depth) depth = r;
-//        System.out.println(node.val + ":::: " + l + "," + r + ">>" + depth);
-//
-//        if(r == depth || (l > depth)) list.add(node.val);
-//
-//        traverse(node.right, 0, r+1);
-//        traverse(node.left, l+1, 0);
-//    }
 }
 
 class LC199Test {
     public static void main(String[] args) {
         LC199 lc199 = new LC199();
         TreeNode root = new TreeNode();
-        //root = root.prepareTree(new Integer[]{1, 2, 3, null, 5, null, 4});
-        //root = root.prepareTree(new Integer[]{1,2,3,4});
-        //root = root.prepareTree(new Integer[]{1,null,3});
-//        root = root.prepareTree(new Integer[]{1,2});
+        root = root.prepareTree(new Integer[]{1, 2, 3, null, 5, null, 4});
+        root = root.prepareTree(new Integer[]{1,2,3,4});
+        root = root.prepareTree(new Integer[]{1,null,3});
+        root = root.prepareTree(new Integer[]{1,2});
         root = root.prepareTree(new Integer[]{1,2,3,null,null,4});
         List<Integer> result = lc199.rightSideView(root);
         for(Integer r : result) System.out.print(r + " ");
