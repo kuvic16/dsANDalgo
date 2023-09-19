10401a8405183a548c3a1d4c85427822f59f3ca9
Shaiful Islam Palash <kuvic16@gmail.com>
2023-09-19 07:03:19 +0600

    LC236 - in progress

--- src/main/java/leetcode/binary_tree/dfs/LC236.java
+++ src/main/java/leetcode/binary_tree/dfs/LC236.java
@@ -6,27 +6,29 @@ public class LC236 {
     public TreeNode t = null;
     public TreeNode tp = null;
     public TreeNode tq = null;
+    public boolean pfound = false;
+    public boolean qfound = false;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         tp = p;
         tq = q;
         traverse(root, "");
-        return root;
+        return t;
     }
 
     public void traverse(TreeNode node, String rail){
         if(node == null) return;
 
-        if(!rail.isEmpty()) rail += ",";
-        rail += node.val;
+        if(compare(node, tp)) pfound = true;
+        if(compare(node, tq)) qfound = true;
+        if(pfound == false && qfound == false) t = node;
+        if((pfound || qfound) && t == null) t = node;
+        //if((pfound || qfound) && t != null && node.val != t.val) t = node;
+        
 
         System.out.println(node.val + ": " + rail);
         traverse(node.left, rail);
         traverse(node.right, rail);
-
-//        if(compare(node, tp)) {
-//            System.out.println("P found");
-//
-//        }
     }
 
     public boolean compare(TreeNode n1, TreeNode n2) {
@@ -56,12 +58,16 @@ class LC236Test{
         TreeNode p = new TreeNode();
         TreeNode q = new TreeNode();
 
+        // root = root.prepareTree(new Integer[]{3,5,1,6,2,0,8,null,null,7,4});
+        // p = p.prepareTree(new Integer[]{5,6,2,null,null,7,4});
+        // q = q.prepareTree(new Integer[]{1,0,8});
+
         root = root.prepareTree(new Integer[]{3,5,1,6,2,0,8,null,null,7,4});
         p = p.prepareTree(new Integer[]{5,6,2,null,null,7,4});
-        q = q.prepareTree(new Integer[]{1,0,8});
+        q = q.prepareTree(new Integer[]{4});
 
 
         TreeNode result = lc236.lowestCommonAncestor(root, p, q);
-        root.print(root);
+        result.print(result);
     }
 }
