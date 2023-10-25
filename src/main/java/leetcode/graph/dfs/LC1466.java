1e54799518a42419205b732568924e48f3a38c03
Shaiful Islam Palash <kuvic16@gmail.com>
2023-10-25 16:22:27 +0600

    LC1466 - accepted

--- src/main/java/leetcode/graph/dfs/LC1466.java
+++ src/main/java/leetcode/graph/dfs/LC1466.java
@@ -1,13 +1,11 @@
 package leetcode.graph.dfs;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class LC1466 {
     List<List<Integer>> orAdjList = new ArrayList<>();
     List<List<Integer>> adjList = new ArrayList<>();
-    Stack<Integer> visited = new Stack<>();
+    Set<Integer> visited = new HashSet<>();
     int count = 0;
     public int minReorder(int n, int[][] connections) {
         for(int i=0; i<n; i++){
@@ -15,11 +13,10 @@ public class LC1466 {
             adjList.add(new ArrayList<>());
         }
 
-        for(int i=0; i<connections.length; i++){
-            int[] paths = connections[i];
+        for (int[] paths : connections) {
             orAdjList.get(paths[0]).add(paths[1]);
             adjList.get(paths[0]).add(paths[1]);
-            if(!adjList.get(paths[1]).contains(paths[0])){
+            if (!adjList.get(paths[1]).contains(paths[0])) {
                 adjList.get(paths[1]).add(paths[0]);
             }
         }
@@ -28,53 +25,17 @@ public class LC1466 {
     }
 
     public void traverse(int index){
-        if(!visited.contains(index)) {
-            visited.push(index);
-            for (Integer x : adjList.get(index)) {
-                if (!visited.contains(x)) {
-                    List<Integer> adjs = orAdjList.get(x);
-                    if (adjs != null && !adjs.contains(index)) {
-                        count += 1;
-                        adjList.get(x).remove(adjList.get(x).indexOf(index));
-                    }
-                    traverse(x);
+        visited.add(index);
+        for (Integer x : adjList.get(index)) {
+            if (!visited.contains(x)) {
+                List<Integer> adjs = orAdjList.get(x);
+                if (adjs != null && !adjs.contains(index)) {
+                    count += 1;
                 }
+                traverse(x);
             }
         }
     }
-
-//    public Stack<Integer> visited = new Stack<>();
-//    public int path = 0;
-//    public int destination = 0;
-//    int[][] adjList = {};
-//    public int minReorder(int n, int[][] connections) {
-//        adjList = connections;
-//        traverse(0, adjList.length);
-//        return path;
-//    }
-//
-//    public void traverse(int index, int n){
-//        if(index >= n) return;
-//
-//        if(!visited.contains(index)) {
-//            int[] list = adjList[index];
-//            if(list[0] == destination || list[1] == destination){
-//                visited.push(index);
-//                if(list[0] == destination) {
-//                    path += 1;
-//                    destination = list[1];
-//                }else destination = list[0];
-//
-//                traverse(index+1, n);
-//            }else{
-//                if(visited.size() < adjList.length){
-//                    destination = 0;
-//                    traverse(0, n);
-//                }
-//            }
-//        }
-//        traverse(index+1, n);
-//    }
 }
 
 class LC1466Test{
