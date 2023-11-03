539445f06dfd8b3f45cb5a9893451babb379bc99
Shaiful Islam Palash <kuvic16@gmail.com>
2023-11-03 16:05:01 +0600

    LC374 - accepted

--- src/main/java/leetcode/binary_search/LC374.java
+++ src/main/java/leetcode/binary_search/LC374.java
@@ -1,12 +1,23 @@
 package leetcode.binary_search;
 
 public class LC374 {
-    int pick = 0;
+    int pick = 6;
 
     public int guessNumber(int n) {
-        int i = 0, j = n-1;
-        while((i+j/2) > 0){
-            int m = (i+j)/2;
+        int i = 1, j = n;
+        if(n <= 2){
+            for(int k=1; k<=n; k++) {
+                int g = guess(k);
+                if (g == 0) return k;
+            }
+            return 0;
+        }
+        while(i <= j){
+            int m = i + (j-i)/2;
+            int g = guess(m);
+            if(g == 0) return m;
+            if(g == -1) j = m - 1;
+            else i = m + 1;
         }
         return 0;
     }
