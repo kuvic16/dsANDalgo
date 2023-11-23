2a1ac057f537a73c4929f840a64e135c8123bd3b
Shaiful Islam Palash <kuvic16@gmail.com>
2023-11-23 12:42:38 +0600

    LC399 - in progress

--- src/main/java/leetcode/graph/dfs/LC399.java
+++ src/main/java/leetcode/graph/dfs/LC399.java
@@ -1,28 +1,57 @@
 package leetcode.graph.dfs;
 
+import java.util.*;
+
 public class LC399 {
-    int[] par = {};
-    public void disjointSet(){
-        par = new int[]{1, 2, 3, 4, 5};
-        
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        double[] result = {};
+
+        Map<String, List<Object[]>> adjList = new HashMap<>();
+        for(int i=0; i<equations.size(); i++){
+            double value = values[i];
+            String source = equations.get(i).get(0);
+            String destination = equations.get(i).get(1);
+
+            Object[] edgeAndCost = {destination, value};
+            Object[] reverseEdgeAndCost = {source, 1/value};
+
+            // source -> destination
+            if(adjList.containsKey(source)){
+                adjList.get(source).add(edgeAndCost);
+            }else{
+                List<Object[]> adj = new ArrayList<>();
+                adj.add(edgeAndCost);
+                adjList.put(source, adj);
+            }
+
+            // destination -> source
+            if(adjList.containsKey(destination)){
+                adjList.get(destination).add(reverseEdgeAndCost);
+            }else{
+                List<Object[]> adj = new ArrayList<>();
+                adj.add(reverseEdgeAndCost);
+                adjList.put(destination, adj);
+            }
+        }
+
+        return result;
     }
 
-    public void union(int a, int b){
-        int u = find(a);
-        int v = find(b);
-        if(u == v) System.out.println("They are already fiends");
-        else par[u]=v;
-    }
-
-    public int find(int r){
-        if(par[r] == r) return r;
-        return find(par[r]);
-    }
-}
-
-class LC399Test {
     public static void main(String[] args){
         LC399 lc399 = new LC399();
-        lc399.disjointSet();
+        List<List<String>> equations = new ArrayList<>();
+        equations.add(Arrays.stream(new String[]{"a", "b"}).toList());
+        equations.add(Arrays.stream(new String[]{"b", "c"}).toList());
+        double[] values = {2, 3};
+        List<List<String>> queries = new ArrayList<>();
+        queries.add(Arrays.stream(new String[]{"a", "c"}).toList());
+        queries.add(Arrays.stream(new String[]{"b", "a"}).toList());
+        queries.add(Arrays.stream(new String[]{"a", "e"}).toList());
+        queries.add(Arrays.stream(new String[]{"a", "a"}).toList());
+        queries.add(Arrays.stream(new String[]{"x", "x"}).toList());
+
+        double[] result = lc399.calcEquation(equations, values, queries);
+
     }
 }
