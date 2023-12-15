29996c29246e167ce6a850274badc9ab732506c3
Shaiful Islam Palash <kuvic16@gmail.com>
2023-12-15 19:13:51 +0600

    LC2462

---
+++ src/main/java/leetcode/heap_priority_queue/LC2462.java
@@ -0,0 +1,18 @@
+package leetcode.heap_priority_queue;
+
+public class LC2462 {
+    public long totalCost(int[] costs, int k, int candidates) {
+        return 0;
+    }
+
+    public static void main(String[] args){
+        LC2462 lc2462 = new LC2462();
+        int[] costs = {};
+        int k = 0, candidates = 0;
+
+        costs = new int[]{17,12,10,2,7,2,11,20,8};
+        k = 3; candidates = 4;
+
+        System.out.println(lc2462.totalCost(costs, k, candidates));
+    }
+}
