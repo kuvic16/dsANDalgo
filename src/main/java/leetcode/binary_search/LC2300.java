259108188906c2066ad4f030bfbdcb03316020b7
Shaiful Islam Palash <kuvic16@gmail.com>
2023-12-20 01:18:22 +0600

    lc2300 in progress

---
+++ src/main/java/leetcode/binary_search/LC2300.java
@@ -0,0 +1,19 @@
+package leetcode.binary_search;
+
+public class LC2300 {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        return new int[]{};
+    }
+
+    public static void main(String[] args){
+        LC2300 lc2300 = new LC2300();
+        int[] spells = {};
+        int[] potions = {};
+        long success = 0;
+
+        spells = new int[]{5,1,3};
+        potions = new int[]{1,2,3,4,5};
+        success = 7;
+
+    }
+}
