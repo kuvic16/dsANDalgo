b46d628234045828950e6919ec5d73ceaa28c560
Shaiful Islam Palash <kuvic16@gmail.com>
2024-01-11 15:37:17 +0600

    at last accepted

--- src/main/java/leetcode/binary_search/LC875.java
+++ src/main/java/leetcode/binary_search/LC875.java
@@ -10,28 +10,25 @@ public class LC875 {
         int max = piles[piles.length-1];
         int min = 0;
         int min_h = Integer.MAX_VALUE;
-        int th = 0;
-        int mid = max;
-        while(max - min > 1){
 
-            th = getHour(piles, mid);
-            System.out.println(mid + ": " + th);
+        while(max - min > 1){
+            int mid = min + (max - min)/2;
+            long th = getHour(piles, mid);
+
+            System.out.println(min + "," + max + "; : mid: " + mid + "; th: " + th);
 
             if(th <= h && min_h > mid) min_h = mid;
             if(th <= h) max = mid;
-            //else if(th > h) min = mid;
             else min = mid;
-            //else min = min + 1;
-
-            mid = min + (max - min)/2;
         }
+        if(min_h == Integer.MAX_VALUE) min_h = max;
 
 
         return  min_h;
     }
 
-    public int getHour(int[] piles, int k){
-        int h = 0;
+    public long getHour(int[] piles, int k){
+        long h = 0;
         for (int pile : piles) {
             h += (int) Math.ceil((double) pile / k);
         }
@@ -53,8 +50,8 @@ public class LC875 {
 //        piles = new int[]{332484035,524908576,855865114,632922376,222257295,690155293,112677673,679580077,337406589,290818316,877337160,901728858,679284947,688210097,692137887,718203285,629455728,941802184};
 //        h = 823855818;
 
-//        piles = new int[]{30,11,23,4,20};
-//        h = 5;
+        piles = new int[]{30,11,23,4,20};
+        h = 5;
 
 //        piles = new int[]{30,11,23,4,20};
 //        h = 6;
@@ -65,8 +62,11 @@ public class LC875 {
 //        piles = new int[]{10};
 //        h = 9;
 
-        piles = new int[]{1000000000,1000000000};
-        h = 3;
+//        piles = new int[]{1000000000,1000000000};
+//        h = 3;
+//
+        piles = new int[]{805306368,805306368,805306368};
+        h = 1000000000;
 
         System.out.println(lc875.minEatingSpeed(piles, h));
     }
