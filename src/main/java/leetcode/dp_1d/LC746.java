e6ed95a46142564a864a457b4b9ee2baad859eb3
Shaiful Islam Palash <kuvic16@gmail.com>
2024-02-09 23:09:51 +0600

    LC746 - Is in progress

---
+++ src/main/java/leetcode/dp_1d/LC746.java
@@ -0,0 +1,14 @@
+package leetcode.dp_1d;
+
+public class LC746 {
+    public int minCostClimbingStairs(int[] cost) {
+        return 0;
+    }
+
+    public static void main(String[] args){
+        LC746 lc746 = new LC746();
+        int[] cost = {};
+        cost = new int[]{10,15,20};
+        System.out.println(lc746.minCostClimbingStairs(cost));
+    }
+}
