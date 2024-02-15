e52cd85a6ea304028108eb4541067ad3aa902f29
Shaiful Islam Palash <kuvic16@gmail.com>
2024-02-15 15:13:06 +0600

    LC746 - accepted

--- src/main/java/leetcode/dp_1d/LC746.java
+++ src/main/java/leetcode/dp_1d/LC746.java
@@ -1,49 +1,37 @@
 package leetcode.dp_1d;
 
 public class LC746 {
-    int min_cost = Integer.MAX_VALUE;
-    int[] disk = new int[1000];
+    int[] disk = new int[10];
 
     public int minCostClimbingStairs(int[] cost) {
-        int r = traverse(cost, 0, 0);
-        disk = new int[1000];
-        int rr = traverse(cost, 1, 0);
-        //System.out.println(r);
-        //traverse(cost, 1, 0);
-        return min_cost;
+        for(int i=cost.length-1; i>=0; i--){
+            disk[i] = -1;
+            traverse(cost, i, 0);
+            //System.out.println(a1);
+            //for(int x : disk) System.out.print(x + " ");
+            //System.out.println();
+        }
+        return Math.min(disk[0], disk[1]);
     }
 
-    public int traverse(int[] cost, int index, int total_cost){
-        if(index >= cost.length) {
-            if(min_cost > total_cost) min_cost = total_cost;
-            return min_cost;
+    public int traverse(int[] cost, int index, int total_cost) {
+        if (index >= cost.length) {
+            return total_cost;
         }
-
-        if(disk[index] == 0) {
-            total_cost += cost[index];
-            traverse(cost, index + 1, total_cost);
-            traverse(cost, index + 2, total_cost);
-
-            //disk[index] = min_cost;
+        if (disk[index] == -1) {
+            int m = traverse(cost, index + 1, total_cost + cost[index]);
+            int n = traverse(cost, index + 2, total_cost + cost[index]);
+            disk[index] = Math.min(m, n);
         }
-        return disk[index];
+        return total_cost + disk[index];
     }
-
-//    public void traverse(int[] cost, int index, int total_cost){
-//        if(index >= cost.length) {
-//            if(min_cost > total_cost) min_cost = total_cost;
-//            return;
-//        }
-//        total_cost += cost[index];
-//        traverse(cost, index + 1, total_cost);
-//        traverse(cost, index + 2, total_cost);
-//    }
 
     public static void main(String[] args){
         LC746 lc746 = new LC746();
         int[] cost = {};
-        //cost = new int[]{10,15,20};
+        cost = new int[]{10,15,20};
         cost = new int[]{1,100,1,1,1,100,1,1,100,1};
+        cost = new int[]{0,0,1,0};
         System.out.println(lc746.minCostClimbingStairs(cost));
     }
 }
