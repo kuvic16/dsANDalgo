69340124a1183ee2a69e91894ed987979f26d79a
Shaiful Islam Palash <kuvic16@gmail.com>
2024-02-15 18:01:42 +0600

    LC198 - In progress

--- src/main/java/leetcode/dp_1d/LC198.java
+++ src/main/java/leetcode/dp_1d/LC198.java
@@ -4,9 +4,21 @@ public class LC198 {
 
     int[] disk = new int[100];
     public int rob(int[] nums) {
-        for(int i=0; i<disk.length; i++) disk[i] = -1;
+        for(int i=nums.length-1; i>=0; i--) {
+            disk[i] = -1;
+            traverse(nums, i, 0);
+        }
+        return Math.max(disk[0], disk[1]);
+    }
 
-        return 0;
+    public int traverse(int[] nums, int index, int total_money){
+        if(index <= 0) return total_money;
+        if(disk[index] == -1){
+            int m = traverse(nums, index + 2, total_money + nums[index]);
+            int n = traverse(nums, index + 3, total_money + nums[index]);
+            disk[index] = Math.max(m, n);
+        }
+        return disk[index] + total_money;
     }
 
 
@@ -17,6 +29,7 @@ public class LC198 {
 
         nums = new int[]{1,2,3,1};
         nums = new int[]{2,7,9,3,1};
+        nums = new int[]{2,1,1,2};
 
         System.out.println(lc198.rob(nums));
     }
