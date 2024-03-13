07fe1c4e0e94eaff38079c7b66200ca0bb2b554d
Shaiful Islam Palash <kuvic16@gmail.com>
2024-03-14 01:40:48 +0600

    LC62 - is in progress

--- src/main/java/leetcode/dp_2d/LC62.java
+++ src/main/java/leetcode/dp_2d/LC62.java
@@ -5,10 +5,16 @@ public class LC62 {
     int _m = 0;
     int _n = 0;
     int sum = 0;
-    int[][] disk = new int[100][1];
+    int[][] disk = new int[3][2];
     public int uniquePaths(int m, int n) {
         _m = m; _n = n;
         nextMove(0, 0);
+        for(int i=0; i<3; i++){
+            for(int j=0; j<2; j++){
+                System.out.print(disk[i][j] + " ");
+            }
+            System.out.println();
+        }
         return sum;
     }
 
@@ -17,15 +23,25 @@ public class LC62 {
         if(_m <= i || _n <= j) return 0;
 
         int r = nextMove(i, j+1);
+        if(r == 1) {
+            System.out.println("i: " + i + ", j: " + j);
+            disk[i][j+1] += 1;
+            return 1;
+        }
         int b = nextMove(i+1, j);
+        if(b == 1) {
+            System.out.println("i: " + i + ", j: " + j);
+            disk[i+1][j] += 1;
+            return 1;
+        }
         return 0;
     }
 
     public static void main(String[] args){
         LC62 lc62 = new LC62();
-        //System.out.println(lc62.uniquePaths(3, 2));
+        System.out.println(lc62.uniquePaths(3, 2));
         //System.out.println(lc62.uniquePaths(3, 7));
         //System.out.println(lc62.uniquePaths(23, 12));
-        System.out.println(lc62.uniquePaths(19, 13));
+        //System.out.println(lc62.uniquePaths(19, 13));
     }
 }
