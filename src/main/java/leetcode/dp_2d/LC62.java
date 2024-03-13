c9db2248abc6c499888b3d9b9f2328e4e978c301
Shaiful Islam Palash <kuvic16@gmail.com>
2024-03-13 16:08:14 +0600

    LC62 - is in progress

--- src/main/java/leetcode/dp_2d/LC62.java
+++ src/main/java/leetcode/dp_2d/LC62.java
@@ -5,23 +5,27 @@ public class LC62 {
     int _m = 0;
     int _n = 0;
     int sum = 0;
+    int[][] disk = new int[100][1];
     public int uniquePaths(int m, int n) {
         _m = m; _n = n;
         nextMove(0, 0);
         return sum;
     }
 
-    public void nextMove(int i, int j){
-        if(_m-1 == i && _n-1 == j) sum += 1;
-        if(_m <= i || _n <= j) return;
+    public int nextMove(int i, int j){
+        if(_m-1 == i && _n-1 == j) {sum += 1; return 1;}
+        if(_m <= i || _n <= j) return 0;
 
-        nextMove(i, j+1);
-        nextMove(i+1, j);
+        int r = nextMove(i, j+1);
+        int b = nextMove(i+1, j);
+        return 0;
     }
 
     public static void main(String[] args){
         LC62 lc62 = new LC62();
         //System.out.println(lc62.uniquePaths(3, 2));
-        System.out.println(lc62.uniquePaths(3, 7));
+        //System.out.println(lc62.uniquePaths(3, 7));
+        //System.out.println(lc62.uniquePaths(23, 12));
+        System.out.println(lc62.uniquePaths(19, 13));
     }
 }
