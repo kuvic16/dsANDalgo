cdbf9b889569f1dd3500683cbae2b1784f2bff84
Shaiful Islam Palash <kuvic16@gmail.com>
2026-01-11 02:30:28 +0600

    LC424 is in progress

---
+++ src/main/java/lc/LC424.java
@@ -0,0 +1,22 @@
+package lc;
+
+import leetcode.binary_tree.TraverseTree.TreeNode;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class LC424 {
+    public int characterReplacement(String s, int k) {
+        return 0;
+    }
+}
+
+class LC424Test {
+    public static void main(String[] args) throws FileNotFoundException {
+        LC424 lc424 = new LC424();
+
+        String s = "ABAB"; int k = 2;
+        int result = lc424.characterReplacement(s, k);
+        System.out.println(result);
+    }
+}
