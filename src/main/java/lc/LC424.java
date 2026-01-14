61f2e4c76c2756e567d1488b1a5213306eefa26e
Shaiful Islam Palash <kuvic16@gmail.com>
2026-01-14 07:09:41 +0600

    LC424 is in progress

--- src/main/java/lc/LC424.java
+++ src/main/java/lc/LC424.java
@@ -10,7 +10,36 @@ import java.util.List;
 
 public class LC424 {
 
+
+    // AABABBA - 1
+    // AABABAA
+    // ABBB
     public int characterReplacement(String s, int k) {
+        if(s.length() <= 1) return s.length();
+
+        int right = 0, left = 0, count = 0, max = 1, kc = 0;
+        char lc = 'B';
+        while(left < s.length() && right < s.length()){
+            if(lc == s.charAt(right)){
+                count++;
+                right++;
+            } else if(lc != s.charAt(right) && kc < k){
+                count++;
+                kc++;
+                left = right;
+                right++;
+            } else if(lc != s.charAt(right) && kc >= k){
+                left = right;
+                right++;
+                max = Math.max(count, max);
+                kc = 0;
+                count = 0;
+            }
+        }
+        return Math.max(max, count);
+//        return 0;
+    }
+    public int XcharacterReplacement(String s, int k) {
         if(s.length() <= 1) return s.length();
 
         int right = 1, left = 0, count =1, max = 1;
@@ -37,7 +66,8 @@ class LC424Test {
 
         //String s = "ABAB"; int k = 2;
         //String s = "AABABBA"; int k = 1;
-        String s = "ABBBABBAABBBBvvvvvvvvBBB"; int k = 2;
+        String s = "ABBB"; int k = 2;
+        //String s = "ABBBABBAABBBBvvvvvvvvBBB"; int k = 2;
         int result = lc424.characterReplacement(s, k);
         System.out.println(result);
     }
