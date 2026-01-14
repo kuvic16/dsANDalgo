6c4ff7751d6e9f848935cafd3edc14ec557ea449
Shaiful Islam Palash <kuvic16@gmail.com>
2026-01-14 12:05:33 +0600

    LC424 is in progress

--- src/main/java/lc/LC424.java
+++ src/main/java/lc/LC424.java
@@ -3,10 +3,7 @@ package lc;
 import leetcode.binary_tree.TraverseTree.TreeNode;
 
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 
 public class LC424 {
 
@@ -14,30 +11,51 @@ public class LC424 {
     // AABABBA - 1
     // AABABAA
     // ABBB
+    // AABABBABBB
     public int characterReplacement(String s, int k) {
         if(s.length() <= 1) return s.length();
 
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(Character c : s.toCharArray()){
+            //map.put(c, map.getOrDefault(c, 0) + 1);
+            map.put('S', 1);
+        }
+
         int right = 0, left = 0, count = 0, max = 1, kc = 0;
-        char lc = 'B';
-        while(left < s.length() && right < s.length()){
-            if(lc == s.charAt(right)){
-                count++;
-                right++;
-            } else if(lc != s.charAt(right) && kc < k){
-                count++;
-                kc++;
-                left = right;
-                right++;
-            } else if(lc != s.charAt(right) && kc >= k){
-                left = right;
-                right++;
-                max = Math.max(count, max);
-                kc = 0;
-                count = 0;
+        // AABABBABBB
+        // AAAB
+        // SDSSMESSTR
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            // System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+            char lc = entry.getKey();
+            right = 0; left = -1; count = 0; kc = 0;
+            
+            while(right < s.length()){
+                if(lc == s.charAt(right)){
+                    count +=1;
+                    right +=1;
+                } else if(lc != s.charAt(right) && kc < k){
+                    count +=1;
+                    kc +=1;
+                    if(left == -1) left = right;
+                    else left = Math.min(left, right);
+                    right +=1;
+                } else if(lc != s.charAt(right) && kc >= k && left != -1){
+                    right = left + 1;
+                    max = Math.max(count, max);
+                    kc = 0;
+                    count = 0;
+                }else{
+                    right++;
+                    max = Math.max(count, max);
+                    kc = 0;
+                    count = 0;
+                }
             }
+            max = Math.max(max, count);
         }
         return Math.max(max, count);
-//        return 0;
     }
     public int XcharacterReplacement(String s, int k) {
         if(s.length() <= 1) return s.length();
@@ -65,9 +83,13 @@ class LC424Test {
         LC424 lc424 = new LC424();
 
         //String s = "ABAB"; int k = 2;
-        //String s = "AABABBA"; int k = 1;
-        String s = "ABBB"; int k = 2;
-        //String s = "ABBBABBAABBBBvvvvvvvvBBB"; int k = 2;
+        //String s = "AABABBABBB"; int k = 1;
+        //String s = "ABBB"; int k = 2;
+        // String s = "ABBBABBAABBBBvvvvvvvvBBB"; int k = 2;
+        //String s = "AAAB"; int k = 0;
+        // String s = "AABA"; int k = 0;
+        //String s = "IMNJJTRMJEGMSOLSCCQICIHLQIOGBJAEHQOCRAJQMBIBATGLJDTBNCPIFRDLRIJHRABBJGQAOLIKRLHDRIGERENNMJSDSSMESSTR"; int k = 2;
+        String s = "SDSSMESSTR"; int k = 2;
         int result = lc424.characterReplacement(s, k);
         System.out.println(result);
     }
