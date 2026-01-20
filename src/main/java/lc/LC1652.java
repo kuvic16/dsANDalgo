0271daeba48af6188926e76c72508a968c831e8c
Shaiful Islam Palash <kuvic16@gmail.com>
2026-01-20 21:43:57 +0600

    LC1652 is in progress

---
+++ src/main/java/lc/LC1652.java
@@ -0,0 +1,18 @@
+package lc;
+
+public class LC1652 {
+    public int[] decrypt(int[] code, int k) {
+        int[] result = {};
+        return result;
+    }
+}
+
+class LC1652Test {
+    public static void main(String[] args){
+        LC1652 lc1652 = new LC1652();
+        int[] code = {};
+        int k = 1;
+        int[] result = lc1652.decrypt(code, k);
+        for(int i : result) System.out.print(result[i] + " ");
+    }
+}
