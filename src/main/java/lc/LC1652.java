796b4e8b614a6ffef73dff1f7a7bbb559c372f96
Shaiful Islam Palash <kuvic16@gmail.com>
2026-01-21 00:22:52 +0600

    LC1652 is in progress

--- src/main/java/lc/LC1652.java
+++ src/main/java/lc/LC1652.java
@@ -1,8 +1,37 @@
 package lc;
 
+import java.util.Arrays;
+
 public class LC1652 {
     public int[] decrypt(int[] code, int k) {
-        int[] result = {};
+        int[] result = new int[code.length];
+        if(k == 0){
+            Arrays.fill(result, 0);
+            return result;
+        }
+        int ksum = 0;
+        if(k > 0) {
+            for (int i = 1; i < 1 + k; i++) ksum += code[i];
+            result[0] = ksum;
+            for (int i = 1; i < code.length; i++) {
+                if (i + k < code.length) ksum = ksum - code[i] + code[i + k];
+                else ksum = ksum - code[i] + code[i + k - code.length];
+                result[i] = ksum;
+            }
+        }else{
+            for (int i = code.length-1; i > code.length - 1 + k; i--) ksum += code[i];
+            result[0] = ksum;
+            for (int i = 1; i < code.length; i++) {
+                if (i-1+k+code.length < code.length) ksum = ksum + code[i-1] - code[i-1+k+code.length];
+                else ksum = ksum - code[i] - code[code.length - i -1 -k];
+                result[i] = ksum;
+            }
+
+            // [2,4,9,3], k = -2 1 -2 = -1 + 4 = 3 1-1-2+4 =2 2-1-2+4 = 3 3-1-2+4=4 4-1
+            //
+            // 1 - (-2) - 3 = 0
+        }
+
         return result;
     }
 }
@@ -10,9 +39,11 @@ public class LC1652 {
 class LC1652Test {
     public static void main(String[] args){
         LC1652 lc1652 = new LC1652();
-        int[] code = {};
-        int k = 1;
+//        int[] code = {5,7,1,4};
+//        int k = 3;
+        int[] code = {2,4,9,3};
+        int k = -2;
         int[] result = lc1652.decrypt(code, k);
-        for(int i : result) System.out.print(result[i] + " ");
+        for(int i : result) System.out.print(i + " ");
     }
 }
