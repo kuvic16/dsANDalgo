3b1d172d627a1da27e9fe1866e5b6ccc8bdb88ef
Shaiful Islam Palash <kuvic16@gmail.com>
2026-02-03 14:43:48 +0600

    LC3258 accepted

--- src/main/java/lc/LC3258.java
+++ src/main/java/lc/LC3258.java
@@ -2,10 +2,14 @@ package lc;
 
 public class LC3258 {
     public int countKConstraintSubstrings(String s, int k) {
-        int n = s.length(), count = 0;
+        int n = s.length(), count = 0, onecount = 0, zerocount = 0;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(s.charAt(i) != s.charAt(j)) count++;
+            onecount = 0; zerocount = 0;
+            for(int j=i; j<n; j++){
+                if(s.charAt(j) == '0') zerocount++;
+                else onecount++;
+                if(onecount <= k || zerocount <= k) count++;
+                if(onecount > k && zerocount > k) break;
             }
         }
         return count;
@@ -15,7 +19,9 @@ public class LC3258 {
 class LC3258Test {
     public static void main(String[] args){
         LC3258 lc3258 = new LC3258();
-        String s = "10101"; int k = 1;
+        //String s = "10101"; int k = 1;
+//        String s = "1010101"; int k = 2;
+        String s = "11111"; int k = 1;
         int result = lc3258.countKConstraintSubstrings(s, k);
         System.out.println(result);
     }
