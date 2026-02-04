fcedcc55271ac5a88d0bc802d04fa702b645e5a4
Shaiful Islam Palash <kuvic16@gmail.com>
2026-02-04 11:30:32 +0600

    LC187 accepted

--- src/main/java/lc/LC187.java
+++ src/main/java/lc/LC187.java
@@ -1,56 +1,31 @@
 package lc;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class LC187 {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> out = new ArrayList<>();
         HashMap<String, Integer> map = new HashMap<>();
-        char[] seq = new char[2];
-        int count = 0, i = 0, nexti = 0;
-        while(i<s.length()) {
-            if(seq[0] != '\u0000' && seq[0] != s.charAt(i) && seq[1] != '\u0000' && seq[1] != s.charAt(i)){
-                seq[0] = s.charAt(i);
-                seq[1] = '\u0000';
-                count = 1;
-            }else if(seq[0] == '\u0000'){
-                seq[0] = s.charAt(i);
-                count += 1;
-                nexti = i;
-            }else if(seq[0] == s.charAt(i)) {
-                count += 1;
-            } else if(seq[1] == '\u0000') {
-                seq[1] = s.charAt(i);
-                count += 1;
-                nexti = i;
-            } else if(seq[1] == s.charAt(i)){
-                count += 1;
-            }
-            if(count == 10){
-                String nseq = s.substring(i-9, i+1);
-                seq[0] = '\u0000';
-                seq[1] = '\u0000';
-                count = 0;
-                if(!map.containsKey(nseq)){
-                    map.put(nseq, 1);
-                    out.add(nseq);
+        HashMap<String, Integer> unq = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            if(s.length() - i >= 10) {
+                String sub = s.substring(i, i + 10);
+                if (map.containsKey(sub)) {
+                    unq.put(sub, 1);
                 }else{
-                    i = nexti;
-                    continue;
+                    map.put(sub, 1);
                 }
             }
-            i++;
         }
-        return out;
+        return new ArrayList<>(unq.keySet());
     }
 }
 
 class LC187Test {
     public static void main(String[] args){
         LC187 lc187 = new LC187();
-        String input = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+//        String input = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        String input = "AAAAAAAAAAAAA";
         List<String> out = lc187.findRepeatedDnaSequences(input);
         for(String o : out) System.out.println(o);
     }
