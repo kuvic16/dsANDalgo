b8135af120401aa88cc622ead69ada796394dd44
Shaiful Islam Palash <kuvic16@gmail.com>
2026-02-21 14:34:51 +0600

    LC904 accepted

--- src/main/java/lc/LC904.java
+++ src/main/java/lc/LC904.java
@@ -2,24 +2,34 @@ package lc;
 
 public class LC904 {
     public int totalFruit(int[] fruits) {
-        int max = Integer.MIN_VALUE;
-        for(int i=0; i<fruits.length; i++){
-            int[] m = new int[2];
-            m[0] = fruits[i]; m[1] = -1;
-            int count = 1;
-            for(int j=i+1; j<fruits.length; j++){
-                if(m[0] == fruits[j]) count++;
-                else if(m[1] == -1){
-                    m[1] = fruits[j];
-                    count++;
-                }else if(m[1] == fruits[j]) count++;
-                else{
-                    max = Math.max(max, count);
-                    break;
+        int max = Integer.MIN_VALUE, lastindex = -1, right = 1;
+        int[] m = new int[2];
+        m[0] = fruits[0]; m[1] = -1;
+        int count = 1, prev = fruits[0];
+        while (right < fruits.length){
+            if(m[0] == fruits[right] || m[1] == fruits[right]) {
+                if(prev != fruits[right]) {
+                    lastindex = right;
+                    prev = fruits[right];
                 }
+                count++; //0,1,6,6,4,4,6
             }
-            max = Math.max(max, count);
+            else if(m[1] == -1){
+                m[1] = fruits[right];
+                count++;
+                lastindex = right;
+                prev = fruits[right];
+            }else{
+                max = Math.max(max, count);
+                m[0] = fruits[lastindex];
+                m[1] = fruits[right];
+                count = right - lastindex + 1;
+                lastindex = right;
+                prev = fruits[right];
+            }
+            right++;
         }
+        max = Math.max(max, count);
         return max == Integer.MIN_VALUE ? 0 : max;
     }
 }
@@ -27,10 +37,13 @@ public class LC904 {
 class LC904Test {
     public static void main(String[] args){
         LC904 lc904 = new LC904();
-        //int[] fruits = {1,2,1};
+//        int[] fruits = {1,2,1};
+//        int[] fruits = {1,0,3,4,3};
+        int[] fruits = {0,1,6,6,4,4,6};
 //        int[] fruits = {0,1,2,2};
-//        int[] fruits = {1,2,3,2,2};
-        int[] fruits = {3,3,3,1,2,1,1,2,3,3,4};
+        //int[] fruits = {1,2,3,2,2};
+        //int[] fruits = {3,3,3,1,2,1,1,2,3,3,4};
+//        int[] fruits = {1,2,3,4};
         int result = lc904.totalFruit(fruits);
         System.out.println(result);
     }
